0effe784539809c2f03b7073fc8a098fe7880b3c
garretMantz <gmantz678@gmail.com>
2018-03-01 10:37:47 -0600

    quick fix

--- travelers/travelers.h
+++ travelers/travelers.h
@@ -5,12 +5,10 @@ using namespace std;
 
 class traveler {
 private:
-	string name;
+	
 public:
 	void play() {
-		cout << "What is your name, traveler?" << endl;
-		cin >> name;
-		cout << "Welcome to Travelers, " << name << ". We've been waiting for someone like you." << endl;
+		
 	}
 
 };
